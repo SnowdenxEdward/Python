cdbe00e884f02befa83bea8a56e400425a3bdaaa
Edward <snowdenxedward@gmail.com>
2024-11-24 17:23:30 +0530

    Create vote.py

---
+++ vote.py
@@ -0,0 +1,12 @@
+# single line comments 
+""" multi
+         line comments
+"""
+Age = int(input('enter your age : '))
+if  Age >= 18 : {
+print('you are eligible for vote')
+
+ } 
+ 
+else:  print('you are minor')
+ 
